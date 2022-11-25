b0a7dbccf63b3518269c89669f7d5903710e81b3
localhorst87 <m.p.ahmann@gmx.de>
2022-11-25 14:33:38 +0100

    added rounding functions

--- Credibility-Development-Kit/util/util-common/index.js
+++ Credibility-Development-Kit/util/util-common/index.js
@@ -3,6 +3,12 @@ const ajv = new Ajv({$data: true, allErrors: true})
 require("ajv-keywords")(ajv);
 
 exports.isStructureValid = isStructureValid
+exports.roundToDigit = roundToDigit;
+exports.getLsd = getLsd;
+exports.roundToInterval = roundToInterval;
+exports.mod = mod;
+
+const PRECISION = 15;
 
 /**
  * Validates if the passed object fulfills the given JSON schema
@@ -16,4 +22,133 @@ exports.isStructureValid = isStructureValid
 function isStructureValid(obj, schema) {
     const validate = ajv.compile(schema);
     return validate(obj);
+}
+
+/**
+ * Applies the given significant digits to the number
+ * 
+ * @author localhorst87
+ * @function 
+ * @param {number} value value to apply precision to
+ * @param {number} precision significant digits
+ * @returns {number} value with given significant digits
+ */
+function precise(value, precision) {
+    return parseFloat(value.toPrecision(precision));
+}
+
+/**
+ * Rounds a value to a given digit.
+ * The function is valid for fast floating point arithmetic,
+ * up to a precision of 15 digits.
+ * 
+ * digit > 0:   digit after decimal point
+ * 
+ * digit == 0:  round to integer
+ * 
+ * digit < 0:   digit before decimal point
+ * 
+ * @author localhorst87
+ * @function
+ * @param {number} value value to round
+ * @param {number} digit the digit to round to
+ * @param {Function} [roundFn=Math.round] round function, Math.round by default
+ * @returns {number} rounded value
+ */
+function roundToDigit(value, digit, roundFn = Math.round) {
+    if (typeof(value) !== "number")
+        throw("value must be numerical");
+    if (typeof(digit) !== "number")
+        throw("digit must be numerical");
+    if (!Number.isInteger(digit))
+        throw("digit must be an integer");
+    if (roundFn !== Math.round && roundFn !== Math.ceil && roundFn !== Math.floor)
+        throw("only Math.round, Math.ceil and Math.floor allowed as roundFn");
+    
+    const tenPower = Math.pow(10, digit);
+    const roundedValue = roundFn(value * tenPower) / tenPower;
+
+    return precise(roundedValue, PRECISION);
+}
+
+/**
+ * Returns the least significant digit of the number.
+ * 
+ * This function is valid for numbers up to a precision
+ * of 15 (numbers with 15 digits without leading or trailing 
+ * zeroes)
+ * 
+ * lsd > 0:   digit after decimal point
+ * 
+ * lsd <= 0:  digit before decimal point
+ * 
+ * @author localhorst87
+ * @function
+ * @param {number} value 
+ * @returns {number} least significant digit
+ */
+function getLsd(value) {
+    if (typeof(value) !== "number")
+        throw("value must be numeric");
+    
+    value = precise(value, PRECISION);
+    
+    let lsd = 0;  
+
+    if (Number.isInteger(value)) {
+        while(value - roundToDigit(value, --lsd, Math.floor) == 0) continue;
+        lsd += 1;
+    }
+    else
+        while(value - roundToDigit(value, ++lsd, Math.floor) > 0) continue;
+    
+    return lsd;
+}
+
+/**
+ * Rounds a number to the nearest number that
+ * can be divided by the given interval
+ * 
+ * @author localhorst87
+ * @function 
+ * @param {number} value raw numeric value
+ * @param {number} interval interval to round to
+ * @returns {number} number with applied interval
+ */
+function roundToInterval(value, interval) {
+    if (typeof(value) !== "number")
+        throw("value must be a number");
+    if (typeof(interval) !== "number")
+        throw("interval must be a number");
+    if (interval <= 0)
+        throw("interval must be > 0");
+    
+    const lsd = Math.max(getLsd(interval), getLsd(value));
+    const remainder = roundToDigit(mod(value, interval), lsd); // use round to digit to avoid floating point errors
+
+    if (value >= 0)
+        return remainder < interval/2 ? roundToDigit(value - remainder, lsd) : roundToDigit(value - remainder + interval, lsd);
+    else 
+        return Math.abs(remainder) <= interval/2 ? roundToDigit(value - remainder, lsd) : roundToDigit(value - remainder - interval, lsd);
+}
+
+/**
+ * floating-point safe modulo operator
+ * 
+ * @author localhorst87
+ * @function
+ * @param {number} value 
+ * @param {number} divisor 
+ * @returns {number} remainder of value / divisor
+ */
+function mod(value, divisor) {
+    if (typeof(value) !== "number")
+        throw("value must be numeric");
+    if (typeof(divisor) !== "number")
+        throw("divisor must be numeric");
+    
+    const lsd = Math.max(getLsd(value), getLsd(divisor));
+    const remainderPower = (value * Math.pow(10, lsd)) % (divisor * Math.pow(10, lsd));
+
+    return remainderPower / Math.pow(10, lsd);
 }
---
+++ Credibility-Development-Kit/util/util-common/test/unit.test.js
@@ -0,0 +1,284 @@
+const { describe, it } = require('mocha');
+const { expect } = require('chai');
+const util = require('..');
+
+describe('roundToDigit', () => {
+
+    describe('argument tests', () => {
+
+        it('all values as expected, no roundFn specified => expect not to throw', () => {
+            expect(() => util.roundToDigit(1.477, 2)).to.not.throw();
+        });
+
+        it('all values as expected, roundFn = Math.ceil => expect not to throw', () => {
+            expect(() => util.roundToDigit(789, -1, Math.ceil)).to.not.throw();
+        });
+
+        it('all values as expected, roundFn = Math.floor => expect not to throw', () => {
+            expect(() => util.roundToDigit(789, -1, Math.floor)).to.not.throw();
+        });
+
+        it('value not number => expect to throw', () => {
+            expect(() => util.roundToDigit([456.44], 1)).to.throw();
+        });
+
+        it('digit not number => expect to throw', () => {
+            expect(() => util.roundToDigit(456.44, [1])).to.throw();
+        });
+
+        it('digit not integer => expect to throw', () => {
+            expect(() => util.roundToDigit(456.44, 1.5)).to.throw();
+        });
+
+        it('roundFn not supported => expect to throw', () => {
+            expect(() => util.roundToDigit(456.44, -2, Math.abs)).to.throw();
+        });
+
+    });
+
+    describe('return test', () => {
+
+        it('digit > 0 => expect to round to value after decimal point', () => {
+            const roundedValue = util.roundToDigit(2500.7137, 3);
+            expect(roundedValue).to.equal(2500.714);
+        });
+
+        it('digit = 0 => expect to round to integer', () => {
+            const roundedValue = util.roundToDigit(-440.10000000002, 0);
+            expect(roundedValue).to.equal(-440);
+        });
+
+        it('digit < 0 => expect to round to power of ten', () => {
+            const roundedValue = util.roundToDigit(2742684.7489, -3);
+            expect(roundedValue).to.equal(2743000);
+        });
+
+        it('roundFn = Math.floor => expect to round down', () => {
+            const roundedValue = util.roundToDigit(9.8447e-2, 2, Math.floor);
+            expect(roundedValue).to.equal(0.09);
+        });
+
+        it('roundFn = Math.ceil => expect to round up', () => {
+            const roundedValue = util.roundToDigit(147123301, -2, Math.ceil);
+            expect(roundedValue).to.equal(147123400);
+        });
+
+        it('floating point representation => expect to cut trailing digits', () => {
+            const roundedValue = util.roundToDigit(0.2-0.02, 2);
+            expect(roundedValue).to.equal(0.18);
+        });
+
+        it('big number => expect to return correct value', () => {
+            const roundedValue = util.roundToDigit(-78e+16, -4, Math.floor);
+            expect(roundedValue).to.equal(-78e16);
+        });
+        
+    });
+    
+});
+
+describe('getLsd', () => {
+
+    describe('argument tests', () => {
+
+        it('value numeric => expect to not throw', () => {
+            expect(() => util.getLsd(-1.2478)).to.not.throw();            
+        });
+
+        it('value not numeric => expect to throw', () => {
+            expect(() => util.getLsd([7.2557])).to.throw();            
+        });
+
+    });
+
+    describe('return tests', () => {
+
+        it('positive value with decimal point => expect lsd > 0', () => {
+            const lsd = util.getLsd(14.7413);
+            expect(lsd).to.equal(4);            
+        });
+
+        it('negative value with decimal point => expect lsd > 0', () => {
+            const lsd = util.getLsd(-0.1110024);
+            expect(lsd).to.equal(7);
+        });     
+
+        it('trailing zeroes after decimal point => expect to ignore trailing zeroes', () => {
+            const lsd = util.getLsd(70.79900);
+            expect(lsd).to.equal(3);
+        });
+
+        it('positive integer value => expect lsd == 0', () => {
+            const lsd = util.getLsd(6503);
+            expect(lsd).to.equal(0);  
+        });
+
+        it('negative integer value => expect lsd == 0', () => {
+            const lsd = util.getLsd(-9654);
+            expect(lsd).to.equal(0);  
+        });
+
+        it('positive integer with trailing zeroes => expect lsd < 0', () => {
+            const lsd = util.getLsd(6500);
+            expect(lsd).to.equal(-2);  
+        });
+
+        it('small number => expect lsd > 0', () => {
+            const lsd = util.getLsd(1.43e-57);
+            expect(lsd).to.equal(59);
+        });
+
+        it('bigger number => expect lsd < 0', () => {
+            const lsd = util.getLsd(-7.8e+14);
+            expect(lsd).to.equal(-13);
+        });
+        
+    });
+    
+});
+
+describe('roundToInterval', () => {
+
+    describe('argument tests', () => {
+
+        it('all args valid => expect to not throw', () => {
+            expect(() => util.roundToInterval(-24.786, 0.05)).to.not.throw();
+        });
+
+        it('value not a number => expect to throw', () => {
+            expect(() => util.roundToInterval('1.4', 0.05)).to.throw();
+        });
+
+        it('interval not a number => expect to throw', () => {
+            expect(() => util.roundToInterval(1.4, '0.05')).to.throw();
+        });
+
+        it('interval 0 => expect to throw', () => {
+            expect(() => util.roundToInterval(1.4, 0)).to.throw();
+        });
+
+        it('interval < 0 => expect to throw', () => {
+            expect(() => util.roundToInterval(741.4, -5)).to.throw();
+        });
+        
+    });
+
+    describe('return tests', () => {
+
+        it('positive number, interval is integer => expect integer rounding', () => {
+            const rounded = util.roundToInterval(24.786, 2);
+            expect(rounded).to.equal(24);
+        });
+
+        it('negative number, interval is integer => expect integer rounding', () => {
+            const rounded = util.roundToInterval(-7424.786, 5);
+            expect(rounded).to.equal(-7425);
+        });
+
+        it('interval < 1 => to round to decimal', () => {
+            const rounded = util.roundToInterval(254.71, 0.5);
+            expect(rounded).to.equal(254.5);
+        });
+
+        it('positive number nearer to smaller number => round down to next interval', () => {
+            const rounded = util.roundToInterval(2.2, 0.5);
+            expect(rounded).to.equal(2.0);
+        });
+
+        it('positive number exactly between two intervals => round up to next interval', () => {
+            const rounded = util.roundToInterval(2.25, 0.5);
+            expect(rounded).to.equal(2.5);
+        });
+
+        it('positive number nearer to higher number => round up to next interval', () => {
+            const rounded = util.roundToInterval(2.3, 0.5);
+            expect(rounded).to.equal(2.5);
+        });
+
+        it('number slightly smaller than being between two intervals => round down to next interval', () => {
+            const rounded = util.roundToInterval(17689.7499999, 0.1);
+            expect(rounded).to.equal(17689.7);
+        });
+
+        it('negative number nearer to higher number => round up to next interval', () => {
+            const rounded = util.roundToInterval(-2.2, 0.5);
+            expect(rounded).to.equal(-2.0);
+        });
+
+        it('negative number exactly between two intervals => round down to next interval', () => {
+            const rounded = util.roundToInterval(-2.25, 0.5);
+            expect(rounded).to.equal(-2.0);
+        });
+
+        it('negative number nearer to smaller number => round up down next interval', () => {
+            const rounded = util.roundToInterval(-2.3, 0.5);
+            expect(rounded).to.equal(-2.5);
+        });
+        
+    });
+    
+});
+
+describe('mod', () => {
+    
+    describe('argument tests', () => {
+        
+        it('all args valid => expect to not throw', () => {
+            expect(() => util.mod(12.5, 0.2)).to.not.throw();
+        });
+
+        it('value not a number => expect to throw', () => {
+            expect(() => util.mod('12.5', 0.2)).to.throw();
+        });
+
+        it('divisor not a number => expect to throw', () => {
+            expect(() => util.mod(12.5, '0.2')).to.throw();
+        });
+
+    });
+
+    describe('return tests', () => {
+
+        it('positive number, positive integer', () => {
+            const rem = util.mod(7.4, 2); 
+            expect(rem).to.equal(1.4); // 1.4000000000000004 for 7.4 % 2
+        });
+
+        it('positive number, negative integer', () => {
+            const rem = util.mod(-45.1e-1, -3); 
+            expect(rem).to.equal(-1.51); // -1.5099999999999998 for -45.1e-1 % -3   
+        });
+
+        it('negative number, positive integer', () => {
+            const rem = util.mod(-58.7, 3); 
+            expect(rem).to.equal(-1.7); // -1.7000000000000028 for -58.7 % 3    
+        });
+
+        it('negative number, negative integer', () => {
+            const rem = util.mod(-147.7, -4); 
+            expect(rem).to.equal(-3.7); // -3.6999999999999886 for -58.7 % -4    
+        });
+        
+        it('positive number, positive decimal point value', () => {
+            const rem = util.mod(2.89, 0.1);
+            expect(rem).to.equal(0.09); // 0.08999999999999997 for 2.89 % 0.1            
+        });
+
+        it('positive number, negative decimal point value', () => {
+            const rem = util.mod(2.89, -0.1);
+            expect(rem).to.equal(0.09); // 0.08999999999999997 for 2.89 % -0.1            
+        });
+
+        it('negative number, positive decimal point value', () => {
+            const rem = util.mod(-28, 0.378);
+            expect(rem).to.equal(-0.028); // -0.027999999999999803 for -28 % 0.378           
+        });
+
+        it('negative number, negative decimal point value', () => {
+            const rem = util.mod(-74, -0.101);
+            expect(rem).to.equal(-0.068); // -0.06799999999999529 for -74 % -0.101     
+        });
+
+    });
+
+});
