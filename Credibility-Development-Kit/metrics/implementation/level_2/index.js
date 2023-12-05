0200fcf0a5699e007b02727d2166e8173325e1d7
localhorst87 <m.p.ahmann@gmx.de>
2023-12-05 10:22:39 +0100

    adapted to new input structure

--- Credibility-Development-Kit/metrics/implementation/level_2/index.js
+++ Credibility-Development-Kit/metrics/implementation/level_2/index.js
@@ -22,9 +22,10 @@ const convergency = require("./src/convergency/convergency");
  * @level 2
  * @phase implementation
  * @step [integration]
- * @param {string} resultExact stringified {@link Measurement} of quasi-exact simulation results
- * @param {...string} resultDiscretized at least 2 stringified {@link Measurement}s of discretized simulation results 
- *                                      with different timesteps
+ * @param {string} signalName the name of the signal to evaluate
+ * @param {number | string} nSamples the number of equidistant samples to evaluate
+ * @param {string} resultExact stringified Signal array as returned by a Signal adapter, like the openmcx-csv-adapter
+ * @param {...string} resultDiscretized at least 2 stringified Signal arrays, as returned by a Signal adapter, like the openmcx-csv-adapter
  * @returns {ResultLog} result and logging information
  */
 const checkConvergency = convergency.verifyConvergency;
--- Credibility-Development-Kit/metrics/implementation/level_2/src/convergency/convergency.js
+++ Credibility-Development-Kit/metrics/implementation/level_2/src/convergency/convergency.js
@@ -13,82 +13,90 @@ exports.verifyConvergency = verifyConvergency;
  * differential equations in the limit of decreasing element size.
  *
  * @author localhorst87
+ * @param {string} signalName the signal where convergency shall be evaluated
+ * @param {number | string} nSamples number of equidistant samples to test
  * @param {string} resultExact
- * @param {...string} resultDiscretized
+ * @param {...string} resultsDiscretized
  * @return {ResultLog}
  */
-function verifyConvergency(resultExact, ...resultDiscretized) {
+function verifyConvergency(signalName, nSamples, resultExact, ...resultsDiscretized) {
+    var signalExact;
     try {
-        let resultExact = JSON.parse(resultExact);
+        resultExact = JSON.parse(resultExact);
+        let signalsExact = resultExact.map(signalString => new Signal(signalString)); // Signal[]
+        signalExact = signalsExact.filter(signal => signal.name == signalName)[0];
     }
     catch (err) {
         return {
             result: false,
-            log: "exact measurement can not be JSON-parsed"
+            log: "exact measurement can not be JSON-parsed, does not fulfill the Signal import schema or Signal is not available"
         };
     }
 
-    if (!util.isStructureValid(resultExact, schemas.MEASUREMENT)) {
-        return {
-            result: false,
-            log: "exact measurement does not fulfill the required JSON schema"
-        };
-    }
-
-    if (resultDiscretized.length < 2) {
+    if (resultsDiscretized.length < 2) {
         return {
             result: false,
             log: "at least 2 discretized measurements must be given"
         };
     }
 
-    let discretizedSolutions = [];
+    let discretizedSolutions = []; // will be of type Signal[][]
 
-    for (let resDiscr of resultDiscretized) {
+    for (let resultDiscretized of resultsDiscretized) {
+        var signalDiscretized;
         try {
-            let resDiscr = JSON.parse(resDiscr);
+            resultDiscretized = JSON.parse(resultDiscretized);
+            let signalsDiscretized = resultDiscretized.map(signalString => new Signal(signalString)); // Signal[]
+            signalDiscretized = signalsDiscretized.filter(signal => signal.name == signalName)[0];
         }
         catch (err) {
             return {
                 result: false,
-                log: "not all discretized measurements can be JSON-parsed"
-            };
-        }
-        if (!util.isStructureValid(resDiscr, schemas.MEASUREMENT)) {
-            return {
-                result: false,
-                log: "not all discretized measurements fulfill the required JSON schema"
+                log: "not all discretized measurements can be JSON-parsed or fulfill the Signal import schema"
             };
         }
 
-        let discretizedSolution = new Signal(resDiscr.time, resDiscr.values, {name: "discretized solution", unit_values: resDiscr.unit});
-        discretizedSolutions.push(discretizedSolution);
+        discretizedSolutions.push(signalDiscretized);
     }
 
     discretizedSolutions.sort((a, b) => b.timestep - a.timestep); // sort array (coarsest mesh first, finest mesh last)
-    let exactSolution = new Signal(resultExact.time, resultExact.values, {name: "exact solution", unit_values: resultExact.unit});
 
     let lastSignal = discretizedSolutions.shift();
-    let diff = lastSignal.subtract(exactSolution);
-    let diffConcreteLast = diff.sliceToIndex(-1);
+    let diffBefore = lastSignal.subtract(signalExact);
 
     while (discretizedSolutions.length > 0) {
         lastSignal = discretizedSolutions.shift();
-        diff = lastSignal.subtract(exactSolution);
-        
-        let diffConcreteNew = diff.sliceToIndex(-1);
-        if (diffConcreteNew.compare("<=", diffConcreteLast, {reduce: true}) == false) {
-            return {
-                result: false,
-                log: "discrete solutions do not converge to exact solution"
-            }
-        }
+        diffNew = lastSignal.subtract(signalExact);
 
-        diffConcreteLast = diffConcreteNew;
+        let timePointsToEvaluate = getEquidistantTimepoints(diffNew.duration, Number(nSamples));
+        valuesToCompareBefore = timePointsToEvaluate.map(t => diffBefore.value(t));
+        valuesToCompareNew = timePointsToEvaluate.map(t => diffNew.value(t));
+
+        for (let i = 0; i < valuesToCompareNew.length; i++) {
+            if (Math.abs(valuesToCompareNew[i]) > Math.abs(valuesToCompareBefore[i])) {
+                return {
+                    result: false,
+                    log: "discrete solutions do not converge to exact solution"
+                }
+            }
+        }                   
+
+        diffBefore = diffNew;
     }
 
     return {
         result: true,
         log: "discrete solutions converge to exact solution"
     };
+}
+
+function getEquidistantTimepoints(duration, nSamples) {
+    let dt = duration / (nSamples + 1);
+    
+    let timepoints = [];
+    for (let i=1; i <= nSamples; i++) {
+        timepoints.push(dt*i);
+    }
+
+    return timepoints;
 }
