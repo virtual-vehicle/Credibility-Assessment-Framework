16c342fce01455978900bc7e292d756ae0ab68cc
localhorst87 <m.p.ahmann@gmx.de>
2023-12-05 10:23:52 +0100

    implemented handy function to change parameters

--- Credibility-Development-Kit/util/ssv-crud/index.js
+++ Credibility-Development-Kit/util/ssv-crud/index.js
@@ -6,3 +6,17 @@ const { SsvCrud } = require('./src/crud');
  * Currently a test release!
  */
 exports.SsvCrud = SsvCrud;
+exports.changeParameter = changeParameter;
+
+/**
+ * @param {string} ssvString 
+ * @param {string} parameterName 
+ * @param {string | number} value 
+ * @returns {string}
+ */
+function changeParameter(ssvString, parameterName, value) {
+    let crud = new SsvCrud(ssvString);
+    crud.setParameterValue(parameterName, Number(value));
+    
+    return crud.export();
+}
