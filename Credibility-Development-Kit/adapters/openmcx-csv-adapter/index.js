efe2330ab54c58be9a8cd204dc42a87170168419
localhorst87 <m.p.ahmann@gmx.de>
2024-01-18 09:48:35 +0100

    add joinTranslatedSignals

--- Credibility-Development-Kit/adapters/openmcx-csv-adapter/index.js
+++ Credibility-Development-Kit/adapters/openmcx-csv-adapter/index.js
@@ -2,13 +2,20 @@ const fs = require('fs');
 const helper = require('./src/helper');
 
 exports.translate = translate;
+exports.joinTranslatedSignals = joinTranslatedSignals;
 
 /**
+ * ...
  * 
- * @param {string} csvPath 
- * @returns {string} an stringified array of stringified Signals
+ * Optionally, if only one Signal of each Signal set should be used to join, use signalToExtract to label the name of 
+ * the Signal you want to extract from each Signal set.
+ * 
+ * @param {string} csvPath path to csv result file
+ * @param {string} [signalToExtract] if only one Signal should be transformed, use signalToExtract to label the name of
+ *                                   the Signal you want to extract
+ * @returns {string} a stringified array of stringified Signals or - if signalToExtract is used - a stringified Signal
  */
-function translate(csvPath) {
+function translate(csvPath, signalToExtract) {
     try {
         var csvString = fs.readFileSync(csvPath, 'utf8');
     }
@@ -18,5 +25,42 @@ function translate(csvPath) {
 
     let signals = helper.extractSignals(csvString);
 
-    return JSON.stringify(signals.map(signal => signal.export()));
+    if (signalToExtract !== undefined) {
+        let filteredSignals = signals.filter(signal => signal.name === signalToExtract);
+        if (filteredSignals.length === 0) 
+            throw(signalToExtract + " does not exist in the result file!");
+    
+        return filteredSignals[0].export();
+    }
+    else {
+        return JSON.stringify(signals.map(signal => signal.export())); 
+    }
+}
+
+/**
+ * A method to join the Signals created by using the translate method.
+ * 
+ * @param {...string} toJoin all Signal sets or Signals to be joined, i.e., each element must be a return value of
+ *                           the translate function
+ * @returns {string} joined, stringified Signal array
+ */
+function joinTranslatedSignals(...toJoin) {
+    let output = [];
+
+    for (let signalOrSignalSet of toJoin) {
+        let toJoinObj = JSON.parse(signalOrSignalSet);
+
+        if (Array.isArray(toJoinObj)) {
+            // element is a stringified exported Signal array
+            // (what comes out of translate without using signalToExtract)
+            for (let exportedSignal of toJoinObj) 
+                output.push(exportedSignal); 
+        }
+        else {
+            // each element is an exported Signal
+            output.push(JSON.stringify(toJoinObj))
+        }
+    }
+
+    return JSON.stringify(output);    
 }
